afdfdb72c241c8198ed71a00212fd205d3b24ede
JoshTrev <joshj438t@gmail.com>
2020-11-05 20:53:11 -0500

    Trying out code for a MERN stack login and authentication.

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const passport = require("passport");
+const users = require("./routes/api/users");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 // Connect to Mongoose
 
@@ -16,13 +48,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/finalProject", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/finalProject", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
