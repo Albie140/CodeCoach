cdbf2749bd632569891b77e84c537ab428e4f792
Seychelle Koplik <sey.koplik@gmail.com>
2020-11-10 11:47:57 -0500

    merging files

--- client/src/components/LearnerPage/LearnerLessonplan.js
+++ client/src/components/LearnerPage/LearnerLessonplan.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import { Card, Button, CardTitle, CardText, Col } from 'reactstrap';
+import React, {useState} from 'react';
+import { Card, Button, CardTitle, CardText, Col, Toast, ToastBody, ToastHeader } from 'reactstrap';
 import './Learner.css'
 
 
 const LearnerLessonplan = (props) => {
+  // const { buttonLabel } = props;
+  const [show, setShow] = useState(false);
 
-    return (
-      
-        <Col sm="6">
-          <Card body className="lessonCard">
-            <CardTitle tag="h5">{props.name}</CardTitle>
-            <CardText>{props.description}</CardText>
-            <CardText>{props.instructions}</CardText>
-            <Button className="lessonBtn"><a href="https://www.w3schools.com/" target="_blank">w3Schools Resource Link</a></Button>
-          </Card>
-        </Col>
-      
-    );
-  };
-  
-  export default LearnerLessonplan;
+  const toggle = () => setShow(!show);
+  return (
+
+    <Col sm="6">
+      <Card body className="lessonCard">
+        <CardTitle tag="h5">{props.name}</CardTitle>
+        <CardText>{props.description}</CardText>
+
+        <CardText>
+          <Button color="primary" onClick={toggle}>Instructions</Button>
+          <Toast isOpen={show}>
+            <ToastHeader toggle={toggle}>Toast title</ToastHeader>
+            <ToastBody>
+              {props.instructions}
+            </ToastBody>
+          </Toast>
+        </CardText>
+
+        
+
+        <Button className="lessonBtn"><a href="https://www.w3schools.com/" target="_blank">w3Schools Resource Link</a></Button>
+      </Card>
+    </Col>
+
+  );
+};
+
+export default LearnerLessonplan;
